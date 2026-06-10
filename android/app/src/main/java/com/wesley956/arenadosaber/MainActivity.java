9053e40dced48b2c7a541991dab57dfb9118b2cf
wesley956 <aweservicosaw@gmail.com>
2026-06-10 08:58:21 +0000

    Add Android Capacitor APK build support

---
+++ android/app/src/main/java/com/wesley956/arenadosaber/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wesley956.arenadosaber;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wesley956.arenadosaber',
+  appName: 'Arena do Saber',
+  webDir: 'dist'
+};
+
+export default config;
